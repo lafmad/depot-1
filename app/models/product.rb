46188241859c56e8f7d76cfa945058cc24d7027c
donboli <wolli.becker@gmail.com>
2014-01-11 15:41:01 -0300

    Line Items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   MIN_PRICE = 0.01
   validates :title, :description, :image_url, presence: true
   validates :title, length: {minimum: 10}
@@ -7,4 +11,17 @@ class Product < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: {
   	with: %r{\.(gif|jpg|png)}, 
   	message: 'must be a URL for GIF, JPG or PNG image.'}
+
+  private
+  
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
